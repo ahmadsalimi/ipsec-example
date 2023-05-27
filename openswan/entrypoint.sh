552aaab88a94204a086f997f737d5f0f92d2927a
ahmadsalimi <ahsa9978@gmail.com>
2023-05-27 18:28:27 +0330

    Configure ipsec

--- openswan/entrypoint.sh
+++ openswan/entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 # sysctl settings
 sysctl -w net.ipv4.ip_forward=1
 sysctl -w net.ipv4.conf.all.accept_redirects=0
@@ -7,3 +9,14 @@ sysctl -w net.ipv4.conf.all.send_redirects=0
 
 # dummy sleep
 sleep infinity
+
+# Check configuration file
+/usr/libexec/ipsec/addconn --config /etc/ipsec.conf --checkconfig
+# Check for kernel modules
+/usr/libexec/ipsec/_stackmanager start
+# Check for nss database status and migration
+/usr/sbin/ipsec --checknss
+# Check for nflog setup
+/usr/sbin/ipsec --checknflog
+# Start the actual IKE daemon
+/usr/libexec/ipsec/pluto --leak-detective --config /etc/ipsec.conf --nofork
